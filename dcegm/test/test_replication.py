5282293373eac8f54f126587bf383673c0bbd888
Sebastian Becker <s3sebeck@uni-bonn.de>
2019-09-11 15:47:12 +0200

    Added new m5 matlab results and fixed parameter specification fr m5

--- dcegm/test/test_replication.py
+++ dcegm/test/test_replication.py
@@ -67,3 +67,66 @@ def test1():
             np.testing.assert_array_almost_equal(
                 policy[period][choice], m0_policy[period][choice]
             )
+
+
+def test2():
+
+    # Load results from the matlab code
+    m5_value = pickle.load(open(TEST_RESOURCES_DIR + "/m5_value_new.pkl", "rb"))
+    m5_policy = pickle.load(open(TEST_RESOURCES_DIR + "/m5_policy_new.pkl", "rb"))
+
+    # Number of periods (fist period is t=1)
+    num_periods = 25
+
+    # Number of grid points over assets
+    num_grid = 500
+
+    # Maximum level of assets
+    mmax = 50
+
+    # Number of quadrature points used in calculation of expectations
+    n_quad_points = 5
+
+    # Interest rate
+    r = 0.05
+
+    # Discount factor
+    beta = 1/(1+r)
+
+    # Standard deviation of log-normally distributed income shocks
+    sigma = 0.35
+
+    # Disutility of work
+    cost_work = 0.35
+
+    # CRRA coefficient (log utility if ==1)
+    theta = 1.95
+
+    coeffs_age_poly = np.array([0.75, 0.04, -0.0002])
+
+    # Scale of the EV taste shocks
+    lambda_ = 0.2
+
+    value, policy = solve_retirement_model(
+        num_grid,
+        n_quad_points,
+        r,
+        coeffs_age_poly,
+        theta,
+        cost_work,
+        beta,
+        lambda_,
+        sigma,
+        mmax,
+        num_periods,
+        cfloor=0.001,
+    )
+
+    for period in range(24):
+        for choice in [1, 0]:
+            np.testing.assert_array_almost_equal(
+                value[period][choice], m5_value[period][choice]
+            )
+            np.testing.assert_array_almost_equal(
+                policy[period][choice], m5_policy[period][choice]
+            )
